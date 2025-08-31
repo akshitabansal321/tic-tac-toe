238c42dac2fefec138c27029a67238e69aff64cf
akshitabansal321 <115450684+akshitabansal321@users.noreply.github.com>
2025-08-31 14:51:13 +0530

    Update code

--- code.cpp
+++ code.cpp
@@ -1,62 +1,55 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <deque>
-
-using namespace std;
+import java.util.*;
 
 // Observer Pattern - for future notification service
-class IObserver {
-public:
-    virtual void update(string msg) = 0;
-    virtual ~IObserver() {}
-};
+interface IObserver {
+    void update(String msg);
+}
 
 // Sample observer implementation
-class ConsoleNotifier : public IObserver {
-public:
-    void update(string msg) override {
-        cout << "[Notification] " << msg << endl;
+class ConsoleNotifier implements IObserver {
+    public void update(String msg) {
+        System.out.println("[Notification] " + msg);
     }
-};
+}
 
 // Symbol/Mark class
 class Symbol {
-private:
-    char mark;
+    private char mark;
     
-public:
-    Symbol(char m) {
+    public Symbol(char m) {
         mark = m;
     }
     
-    char getMark() {
+    public char getMark() {
         return mark;
     }
-};
+}
 
 // Board class - Dumb object that only manages the grid
 class Board {
-private:
-    vector<vector<Symbol*>> grid;
-    int size;
-    Symbol* emptyCell;
+    private Symbol[][] grid;
+    private int size;
+    private Symbol emptyCell;
     
-public:
-    Board(int s) {
+    public Board(int s) {
         size = s;
         emptyCell = new Symbol('-');
-        grid = vector<vector<Symbol*>>(size, vector<Symbol*>(size, emptyCell));
+        grid = new Symbol[size][size];
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
+                grid[i][j] = emptyCell;
+            }
+        }
     }
     
-    bool isCellEmpty(int row, int col) {
+    public boolean isCellEmpty(int row, int col) {
         if(row < 0 || row >= size || col < 0 || col >= size) {
             return false;
         }
         return grid[row][col] == emptyCell;
     }
     
-    bool placeMark(int row, int col, Symbol* mark) {
+    public boolean placeMark(int row, int col, Symbol mark) {
         if(row < 0 || row >= size || col < 0 || col >= size) {
             return false;
         }
@@ -67,101 +60,92 @@ public:
         return true;
     }
     
-    Symbol* getCell(int row, int col) {
+    public Symbol getCell(int row, int col) {
         if(row < 0 || row >= size || col < 0 || col >= size) {
             return emptyCell;
         }
         return grid[row][col];
     }
     
-    int getSize() {
+    public int getSize() {
         return size;
     }
     
-    Symbol* getEmptyCell() {
+    public Symbol getEmptyCell() {
         return emptyCell;
     }
     
-    void display() {
-        cout << "\n  ";
+    public void display() {
+        System.out.print("\n  ");
         for(int i = 0; i < size; i++) {
-            cout << i << " ";
+            System.out.print(i + " ");
         }
-        cout << endl;
+        System.out.println();
         
         for(int i = 0; i < size; i++) {
-            cout << i << " ";
+            System.out.print(i + " ");
             for(int j = 0; j < size; j++) {
-                cout << grid[i][j]->getMark() << " ";
+                System.out.print(grid[i][j].getMark() + " ");
             }
-            cout << endl;
+            System.out.println();
         }
-        cout << endl;
+        System.out.println();
     }
-};
+}
 
 // Player class --> 
 class TicTacToePlayer {
-private:
-    int playerId;
-    string name;
-    Symbol* symbol;
-    int score;
+    private int playerId;
+    private String name;
+    private Symbol symbol;
+    private int score;
     
-public:
-    TicTacToePlayer(int playerId, string n, Symbol* s) {
-        this->playerId = playerId;
+    public TicTacToePlayer(int playerId, String n, Symbol s) {
+        this.playerId = playerId;
         name = n;
         symbol = s;
         score = 0;
     }
     
     // Getters and setters
-    string getName() { 
+    public String getName() { 
         return name; 
     }
 
-    Symbol* getSymbol() { 
+    public Symbol getSymbol() { 
         return symbol; 
     }
 
-    int getScore() { 
+    public int getScore() { 
         return score; 
     }
 
-    void incrementScore() { 
+    public void incrementScore() { 
         score++;
     }
-    
-    ~TicTacToePlayer() {
-        delete symbol;
-    }
-};
+}
 
 // Strategy Pattern for game rules
-class TicTacToeRules {
-public:
-    virtual bool isValidMove(Board* board, int row, int col) = 0;
-    virtual bool checkWinCondition(Board* board, Symbol* symbol) = 0;
-    virtual bool checkDrawCondition(Board* board) = 0;
-    virtual ~TicTacToeRules() {}
-};
+interface TicTacToeRules {
+    boolean isValidMove(Board board, int row, int col);
+    boolean checkWinCondition(Board board, Symbol symbol);
+    boolean checkDrawCondition(Board board);
+}
 
 // Standard Tic Tac Toe rules
-class StandardTicTacToeRules : public TicTacToeRules {
-public:
-    bool isValidMove(Board* board, int row, int col) override {
-        return board->isCellEmpty(row, col);
+class StandardTicTacToeRules implements TicTacToeRules {
+    public boolean isValidMove(Board board, int row, int col) {
+        return board.isCellEmpty(row, col);
     }
     
-    bool checkWinCondition(Board* board, Symbol* symbol) override {
-        int size = board->getSize();
+    public boolean checkWinCondition(Board board, Symbol symbol) {
+        int size = board.getSize();
         
         // Check rows
         for(int i = 0; i < size; i++) {
-            bool win = true;
+            boolean win = true;
             for(int j = 0; j < size; j++) {
-                if(board->getCell(i, j) != symbol) {
+                if(board.getCell(i, j) != symbol) {
                     win = false;
                     break;
                 }
@@ -171,9 +155,9 @@ public:
         
         // Check columns
         for(int j = 0; j < size; j++) {
-            bool win = true;
+            boolean win = true;
             for(int i = 0; i < size; i++) {
-                if(board->getCell(i, j) != symbol) {
+                if(board.getCell(i, j) != symbol) {
                     win = false;
                     break;
                 }
@@ -182,9 +166,9 @@ public:
         }
         
         // Check main diagonal
-        bool win = true;
+        boolean win = true;
         for(int i = 0; i < size; i++) {
-            if(board->getCell(i, i) != symbol) {
+            if(board.getCell(i, i) != symbol) {
                 win = false;
                 break;
             }
@@ -194,7 +178,7 @@ public:
         // Check anti-diagonal
         win = true;
         for(int i = 0; i < size; i++) {
-            if(board->getCell(i, size-1-i) != symbol) {
+            if(board.getCell(i, size-1-i) != symbol) {
                 win = false;
                 break;
             }
@@ -203,152 +187,144 @@ public:
     }
     
     // If all cells are filled and no winner
-    bool checkDrawCondition(Board* board) override {
-        int size = board->getSize();
+    public boolean checkDrawCondition(Board board) {
+        int size = board.getSize();
         for(int i = 0; i < size; i++) {
             for(int j = 0; j < size; j++) {
-                if(board->getCell(i, j) == board->getEmptyCell()) {
+                if(board.getCell(i, j) == board.getEmptyCell()) {
                     return false;
                 }
             }
         }
         return true;
     }
-};
+}
 
 // Game class --> Observable
 class TicTacToeGame {
-private:
-    Board* board;
-    deque<TicTacToePlayer*> players;
-    TicTacToeRules* rules;
-    vector<IObserver*> observers;
-    bool gameOver;
+    private Board board;
+    private Deque<TicTacToePlayer> players;
+    private TicTacToeRules rules;
+    private List<IObserver> observers;
+    private boolean gameOver;
     
-public:
-    TicTacToeGame(int boardSize) {
+    public TicTacToeGame(int boardSize) {
         board = new Board(boardSize);
+        players = new ArrayDeque<>();
         rules = new StandardTicTacToeRules();
+        observers = new ArrayList<>();
         gameOver = false;
     }
     
-    void addPlayer(TicTacToePlayer* player) {
-        players.push_back(player);
+    public void addPlayer(TicTacToePlayer player) {
+        players.addLast(player);
     }
     
-    void addObserver(IObserver* observer) {
-        observers.push_back(observer);
+    public void addObserver(IObserver observer) {
+        observers.add(observer);
     }
 
-    void notify(string msg) {
-        for(auto observer : observers) {
-            observer->update(msg);
+    public void notify(String msg) {
+        for(IObserver observer : observers) {
+            observer.update(msg);
         }
     }
     
-    void play() {
+    public void play() {
         if(players.size() < 2) {
-            cout << "Need at least 2 players!" << endl;
+            System.out.println("Need at least 2 players!");
             return;
         }
         
         notify("Tic Tac Toe Game Started!");
         
+        Scanner scanner = new Scanner(System.in);
+        
         while(!gameOver) {
-            board->display();
+            board.display();
             
             // Take out the current player from dequeue
-            TicTacToePlayer* currentPlayer = players.front();
-            cout << currentPlayer->getName() << " (" << currentPlayer->getSymbol()->getMark() << ") - Enter row and column: ";
+            TicTacToePlayer currentPlayer = players.peekFirst();
+            System.out.print(currentPlayer.getName() + " (" + currentPlayer.getSymbol().getMark() + ") - Enter row and column: ");
             
-            int row, col;
-            cin >> row >> col;
+            int row = scanner.nextInt();
+            int col = scanner.nextInt();
             
             // check if move is valid
-            if(rules->isValidMove(board, row, col)) {
-                board->placeMark(row, col, currentPlayer->getSymbol());
-                notify(currentPlayer->getName() + " played (" + to_string(row) + "," + to_string(col) + ")");
+            if(rules.isValidMove(board, row, col)) {
+                board.placeMark(row, col, currentPlayer.getSymbol());
+                notify(currentPlayer.getName() + " played (" + row + "," + col + ")");
                 
-                if(rules->checkWinCondition(board, currentPlayer->getSymbol())) {
-                    board->display();
-                    cout << currentPlayer->getName() << " wins!" << endl;
-                    currentPlayer->incrementScore();
+                if(rules.checkWinCondition(board, currentPlayer.getSymbol())) {
+                    board.display();
+                    System.out.println(currentPlayer.getName() + " wins!");
+                    currentPlayer.incrementScore();
 
-                    notify(currentPlayer->getName() + " wins!");
+                    notify(currentPlayer.getName() + " wins!");
 
                     gameOver = true;
                 }
-                else if(rules->checkDrawCondition(board)) {
-                    board->display();
+                else if(rules.checkDrawCondition(board)) {
+                    board.display();
                     
-                    cout << "It's a draw!" << endl;
+                    System.out.println("It's a draw!");
                     notify("Game is Draw!");
 
                     gameOver = true;
                 }
                 else {
                     // Move player to back of queue
-                    players.pop_front();
-                    players.push_back(currentPlayer);
+                    players.removeFirst();
+                    players.addLast(currentPlayer);
                 }
             }
             else {
-                cout << "Invalid move! Try again." << endl;
+                System.out.println("Invalid move! Try again.");
             }
         }
     }
-    
-    ~TicTacToeGame() {
-        delete board;
-        delete rules;
-    }
-};
+}
 
 // Enum & Factory Pattern for game creation
 enum GameType {
     STANDARD
-};
+}
 
 class TicTacToeGameFactory {
-public:
-    static TicTacToeGame* createGame(GameType gt, int boardSize) {
-        if(GameType::STANDARD == gt) {
+    public static TicTacToeGame createGame(GameType gt, int boardSize) {
+        if(GameType.STANDARD == gt) {
             return new TicTacToeGame(boardSize);
         }
-        return nullptr;
+        return null;
     }
-};
+}
 
-// Main function for Tic Tac Toe
-int main() {
-    cout << "=== TIC TAC TOE GAME ===" << endl;
-    
-    // Create game with custom board size
-    int boardSize;
-    cout << "Enter board size (e.g., 3 for 3x3): ";
-    cin >> boardSize;
-    
-    TicTacToeGame* game = TicTacToeGameFactory::createGame(GameType::STANDARD, boardSize);
-    
-    // Add observer
-    IObserver* notifier = new ConsoleNotifier();
-    game->addObserver(notifier);
-    
-    // Create players with custom symbols
-    TicTacToePlayer* player1 = new TicTacToePlayer(1, "Aditya", new Symbol('X'));
-    TicTacToePlayer* player2 = new TicTacToePlayer(2, "Harshita", new Symbol('O'));
-    
-    game->addPlayer(player1);
-    game->addPlayer(player2);
-    
-    // Play the game
-    game->play();
-    
-    // Cleanup
-    delete game;
-    delete player1;
-    delete player2;
-    delete notifier;
-    
-    return 0;
+// Main class for Tic Tac Toe
+public class TicTacToeMain {
+    public static void main(String[] args) {
+        System.out.println("=== TIC TAC TOE GAME ===");
+        
+        // Create game with custom board size
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter board size (e.g., 3 for 3x3): ");
+        int boardSize = scanner.nextInt();
+        
+        TicTacToeGame game = TicTacToeGameFactory.createGame(GameType.STANDARD, boardSize);
+        
+        // Add observer
+        IObserver notifier = new ConsoleNotifier();
+        game.addObserver(notifier);
+        
+        // Create players with custom symbols
+        TicTacToePlayer player1 = new TicTacToePlayer(1, "Aditya", new Symbol('X'));
+        TicTacToePlayer player2 = new TicTacToePlayer(2, "Harshita", new Symbol('O'));
+        
+        game.addPlayer(player1);
+        game.addPlayer(player2);
+        
+        // Play the game
+        game.play();
+        
+        scanner.close();
+    }
 }
